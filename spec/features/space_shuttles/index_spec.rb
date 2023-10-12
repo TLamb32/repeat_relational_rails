8bde80f60be9fffc940b1d180253d74ee12e361c
Terry Lamb <terrylamb@gmail.com>
2023-10-12 15:57:36 -0400

    complete us2, refactor test setup to elimintate repeating test data in browser

--- spec/features/space_shuttles/index_spec.rb
+++ spec/features/space_shuttles/index_spec.rb
@@ -1,9 +1,11 @@
 require "rails_helper"
 
 RSpec.describe "Space Shuttles Index" do
+  before :each do
+    @spaceshuttle_1 = SpaceShuttle.create(mission_name: "Endeavour", crew: 8, flight_ready: false)
+    @spaceshuttle_2 = SpaceShuttle.create(mission_name: "Atlantis", crew: 9, flight_ready: true)
+  end
   describe "when a user visits /spaceshuttles" do
-    spaceshuttle_1 = SpaceShuttle.create(mission_name: "Endeavour", crew: 8, flight_ready: false)
-    spaceshuttle_2 = SpaceShuttle.create(mission_name: "Atlantis", crew: 9, flight_ready: true)
     
     # US 1
     it "can display each spaceshuttle record" do
@@ -17,8 +19,9 @@ RSpec.describe "Space Shuttles Index" do
 
       visit "/spaceshuttles"
 
-      expect(page).to have_content(spaceshuttle_1.mission_name)
-      expect(page).to have_content(spaceshuttle_2.mission_name)
+      expect(page).to have_content(@spaceshuttle_1.mission_name)
+      expect(page).to have_content(@spaceshuttle_2.mission_name)
+      # save_and_open_page
     end
   end
 end
